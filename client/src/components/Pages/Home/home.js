5c19179c05463620fde496e738749b38971941ef
Leah <leahxharden@gmail.com>
2023-06-03 13:58:02 -0400

    added the fonts and did format for the sign up pages

--- client/src/components/Pages/Home/home.js
+++ client/src/components/Pages/Home/home.js
@@ -1,10 +1,10 @@
 // import React, { useState } from "react";
-import Header from "../header/header";
-import Footer from "../footer/footer";
+import Header from "../../header/header";
+import Footer from "../../footer/footer";
 import About from "../about/about";
 //import Contact from "./contact";
 
-import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function Home({ setPage }) {
   return (
