759a569e2239e8800791d962c2ac9cb324c0047e
ginitadavis <120149064+ginitadavis@users.noreply.github.com>
2023-06-03 22:21:47 -0400

    Back-end of login page working

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -4,9 +4,10 @@ export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
-      parent {
+      user {
         _id
         name
+        email
       }
     }
   }
@@ -16,7 +17,7 @@ export const ADD_USER = gql`
   mutation addUser($name: String!, $email: String!, $password: String!) {
     addUser(name: $name, email: $email, password: $password) {
       token
-      parent {
+      user {
         _id
         name
         email
--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,12 +1,21 @@
 const db = require('../config/connection');
 const { Parent } = require('../models');
-
+const bcrypt = require('bcrypt');
 const parentData = require('./parentSeed.json');
 
 db.once('open', async () => {
   await Parent.deleteMany({});
 
-  const parents = await Parent.insertMany(parentData);
+  // Iterate over the parentData and hash the passwords before saving them
+  const parents = await Promise.all(parentData.map(async (parent) => {
+    const hashedPassword = await bcrypt.hash(parent.password, 10);
+    return {
+      ...parent,
+      password: hashedPassword
+    };
+  }));
+
+  await Parent.insertMany(parents);
 
   console.log('Parents seeded!');
   process.exit(0);
