28196077035bc844c44ad2f3ef315839d612282a
Leah <leahxharden@gmail.com>
2023-06-06 12:56:40 -0400

    made the badge part

--- client/src/components/Pages/Home/home.js
+++ client/src/components/Pages/Home/home.js
@@ -1,8 +1,27 @@
 
+import Circle from './circlePlaceholder.js'
+import BigCircle from './BigCircle.js';
+
+
 function Home() {
   return (
     <div>
+      <div className='brownBox'>
+        <BigCircle />
+        <h2 className='subName'>Your Name!</h2>
+        <div className='Badges'>
 
+          <Circle />
+          <Circle />
+
+          <Circle />
+          <Circle />
+
+          <Circle />
+          <Circle />
+
+        </div>
+      </div>
 
     </div>
   );
