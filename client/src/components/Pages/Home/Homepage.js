cb3dfa1ad6869875f6a9c4fe62e9324ba3d96764
Leah <leahxharden@gmail.com>
2023-06-06 15:02:00 -0400

    finished front page

--- client/src/components/Pages/Home/Homepage.js
+++ client/src/components/Pages/Home/Homepage.js
@@ -19,18 +19,27 @@ function Home() {
             <div>
                 <div className="brownBox">
                     <Circle />
-                    <h3>Store</h3>
+                    <h3 className='fontsizeHome'>Store</h3>
 
                 </div>
-                <div className="brownBox">
-                    <h3>Streak!</h3>
-                    <h3>Great Job!</h3>
+                <div className="Streak">
+                    <div >
+
+                        <h3>Streak!</h3>
+                        <h3>Great Job!</h3>
+                    </div>
                     <Circle />
                 </div>
 
                 <div className="brownBox">
                     <Circle />
-                    <h3>Calendar</h3>
+                    <h3 className='fontsizeHome'>Calendar</h3>
+
+                </div>
+                <div className="greyBox">
+
+                    <h3>Grownup</h3>
+                    <h3>Area</h3>
 
                 </div>
 
