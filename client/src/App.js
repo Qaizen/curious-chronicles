1293829cb697b25c71f11c7bece6aad566789a1e
ginitadavis <120149064+ginitadavis@users.noreply.github.com>
2023-06-12 19:29:35 -0400

    Fixed bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
+
+
+// // const client = new ApolloClient({
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   createHttpLink,
+// } from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { useQuery } from "@apollo/client";
 import { GET_ME } from "./utils/queries.js"
-
 //adds CSS
 import './Reset.css';
 import './App.css';
@@ -18,7 +27,6 @@ import Home from './components/Pages/Home/Homepage.js';
 import Login from './components/Pages/GrownupArea/Login.js'
 import GrownupArea from './components/Pages/GrownupArea/GrownupArea.js'
 
-
 //question imports
 import QMood from './components/Pages/Questions/QMood.js'
 import QOne from './components/Pages/Questions/QOne.js'
@@ -31,13 +39,16 @@ import { loadStripe } from '@stripe/stripe-js';
 import CheckoutForm from './components/Pages/NewStripe/CheckoutForm';
 
 function App() {
-    const { data } = useQuery(GET_ME);
+
+  //stripe
+  // Make sure to call `loadStripe` outside of a component’s render to avoid
+  // recreating the `Stripe` object on every render.
+  const stripePromise = loadStripe('pk_test_51NGW5hGa0dfhEuOuwIeTm5qdHGSsUA2bk6fgFyDixDKGA8GnqaQYAtJcmbQkwfsRbwAaZjbhP61IXtwkhKiHksbP00i5Iugqla');
+
+  const { data } = useQuery(GET_ME);
   const userMe = data?.me;
 
-//stripe
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
-const stripePromise = loadStripe('pk_test_51NGW5hGa0dfhEuOuwIeTm5qdHGSsUA2bk6fgFyDixDKGA8GnqaQYAtJcmbQkwfsRbwAaZjbhP61IXtwkhKiHksbP00i5Iugqla');
+
 
   // const [current, setCurrent] = useState('/');
   // const location = useLocation();
@@ -45,38 +56,34 @@ const stripePromise = loadStripe('pk_test_51NGW5hGa0dfhEuOuwIeTm5qdHGSsUA2bk6fgF
   // useEffect(() => {
   //   setCurrent(location.pathname);
   // }, [location]);
-//stripe
-  const options = {
-    clientSecret: '{{ CLIENT_SECRET }}',
 
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Frontpage />} />
-            <Route path="/ChildSignup" element={<ChildSignup />} />
-            <Route path="/ParentSignup" element={<ParentSignup />} />
-            <Route path="/Home" element={<Home />} />
-            <Route path="/Login" element={<Login />} />
-            {/* grown up links  */}
-            <Route path="/GrownupArea" element={<GrownupArea />} />
-            {/* questions links  */}
-            <Route path="/QMood" element={<QMood />} />
-            <Route path="/QOne" element={<QOne />} />
-            <Route path="/QTwo" element={<QTwo />} />
-            <Route path="/QFinished" element={<QFinished />} />
-            {/* Stripe Links */}
-            <Route path="/Sub" element={<Sub />} />
-            <Route path="/Checkout">
-              <Elements stripe={stripePromise} options={options}>
-                <CheckoutForm />
-              </Elements>
-            </Route>
-          </Routes>
-        </div>
-      </Router>
-    </ApolloProvider>
+
+
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Frontpage />} />
+          <Route path="/ChildSignup" element={<ChildSignup user={userMe} />} />
+          <Route path="/ParentSignup" element={<ParentSignup />} />
+          <Route path="/Home" element={<Home />} />
+
+
+          {/* grown up links  */}
+          <Route path="/Login" element={<Login />} />
+          <Route path="/GrownupArea" element={<GrownupArea />} />
+          <Route path="/Sub" element={<Sub />} /> {/*Subscription page*/}
+
+          {/* questions links  */}
+          <Route path="/QMood" element={<QMood />} />
+          <Route path="/QOne" element={<QOne />} /> {/*Why do you feel that way page*/}
+          <Route path="/QTwo" element={<QTwo />} /> {/*Thing the kid is good at page*/}
+          <Route path="/QFinished" element={<QFinished />} />
+        </Routes>
+      </div>
+    </Router>
+
+
   );
 }
 
--- client/src/components/Pages/GrownupArea/Login.js
+++ client/src/components/Pages/GrownupArea/Login.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../../../utils/mutations.js';
 
@@ -83,37 +82,6 @@ const Login = (props) => {
                                     value={formState.password}
                                     onChange={handleChange}
                                 />
-                            </div>
-
-                            <button
-                                className="BtnRed"
-                                style={{ cursor: 'pointer' }}
-                                type="submit"
-                            >
-                                Submit
-                            </button>
-
-                        </form>
-                        <a href='/GrownupArea'>
-
-                            <button
-                                className="BtnRed"
-                                style={{ cursor: 'pointer' }}
-                                type="submit"
-                            >
-                                delete later
-                            </button>
-                        </a>
-                    </div>
-                )}
-
-                {error && (
-                    <div className="Mali">
-                        {error.message}
-                    </div>
-                )}
-            </div>
-
                                 <label>
                                     <input
                                         name="showPassword"
@@ -149,4 +117,3 @@ const Login = (props) => {
 };
 
 export default Login;
-
